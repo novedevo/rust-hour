149967ee4d3401a85e654539cadc45dec87dbae7
Devon Burnham <novedevo@gmail.com>
2021-04-07 18:24:06 -0700

    add a test to fully solve all puzzles

--- src/lib.rs
+++ src/lib.rs
@@ -25,4 +25,22 @@ mod tests {
         std::fs::create_dir_all("solutions").unwrap();
         solver::solve("puzzles/B19.txt", "solutions/B19.txt");
     }
+    
+    #[test]
+    fn solve_all() {      
+        std::fs::create_dir_all("solutions").unwrap();
+        let mut threads = vec![];
+        for entry in std::fs::read_dir("puzzles").unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path().to_str().unwrap().to_owned();
+            let name = entry.file_name().to_str().unwrap().to_owned();
+            threads.push(std::thread::spawn(move || {
+                solver::solve(&path, &format!("solutions/{}", name));
+            }));
+        }
+
+        for handle in threads {
+            handle.join().unwrap();
+        }
+    }
 }
